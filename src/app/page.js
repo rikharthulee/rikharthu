931baaa6c06ac4adf1f0f6c3b25af13f8807d1a7
Richard Lee <141524775+rikharthulee@users.noreply.github.com>
2025-10-26 16:25:37 +0700

    initial commit - set up next.js

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 text-gray-900">
+      <h1 className="text-3xl font-semibold">rikharthu.co.uk coming soon 👋</h1>
+    </main>
+  );
+}
